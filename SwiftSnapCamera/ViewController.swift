8b838020fd29f3be9467770fd215e3b4a7e774e5
TaigaMikami <mikamimikami60@gmail.com>
2018-09-23 15:22:47 +0900

    Initial Commit

---
+++ SwiftSnapCamera/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SwiftSnapCamera
+//
+//  Created by 三上大河 on 2018/09/23.
+//  Copyright © 2018年 三上大河. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
